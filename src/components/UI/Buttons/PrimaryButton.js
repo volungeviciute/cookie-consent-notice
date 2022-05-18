3c423b5a97fdb671ccc46035cfb15cb9e1f2529d
volungeviciute <laima.volungeviciute@gmail.com>
2022-05-18 10:04:53 +0300

    cleanup

--- src/components/UI/Buttons/PrimaryButton.js
+++ src/components/UI/Buttons/PrimaryButton.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Button.css'
+import React from 'react';
+import './Button.css';
 
 function PrimaryButton(props) {
   return (
--- src/components/UI/Buttons/SecondaryButton.js
+++ src/components/UI/Buttons/SecondaryButton.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Button.css'
+import React from 'react';
+import './Button.css';
 
 function SecondaryButton(props) {
   return (
